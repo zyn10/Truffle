b7cf86fefceb2513b4535a0c808794701e6e7e31
Muhammad Zain <zainandcs@gmail.com>
2023-06-04 10:55:26 +0500

    Not Integrated

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import SimpleStorage from "./contracts/SimpleStorage.json";
+import Web3 from "web3";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [state, setState] = useState({
+web3: null,
+contract: null,
+});
+const [data, setData] = useState("nill");
+useEffect(() => {
+const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+async function template() {
+const web3 = new Web3(provider);
+const networkId = await web3.eth.net.getId();
+const deployedNetwork = SimpleStorage.networks[networkId];
+const contract = new web3.eth.Contract(
+SimpleStorage.abi,
+deployedNetwork.address
 
+);
+console.log(contract);
+setState({ web3: web3, contract: contract });
+}
+provider && template();
+}, []);
+useEffect(() => {
+const { contract } = state;
+async function readData() {
+const data = await contract.methods.getter().call();
+setData(data);
+}
+contract && readData();
+}, [state]);
+async function writeData() {
+const { contract } = state;
+const data = document.querySelector("#value").value;
+await contract.methods
+.setter(data)
+.send({ from: "0x1f4F90f9aA5779f2C1E190133C2c872944bDED1c" });
+window.location.reload();
+}
+return (
+<>
+<h1>Welcome to Dapp</h1>
+<div className="App">
+<p className="text">Contract Data : {data}</p>
+<div>
+<input type="text" id="value" required="required"></input>
+</div>
+<button onClick={writeData} className="button button2">
+Change Data
+</button>
+</div>
+</>
+);
+}
 export default App;
